0bb2175d961d7ea271f4533bc7ea205f6c8cac07
CC121@ccs.local <CC121@ccs.local>
2019-07-07 05:01:53 +0800

    first commit . Activity 1

---
+++ Database.php
@@ -0,0 +1,24 @@
+<?php
+class Database{
+    private $con;
+    public $servername="localhost";
+    public $username="root";
+    public $password="";
+    public $dbname = "rabor_db";
+
+    public function __construct(){
+        $this->con = mysqli_connect($this->servername, $this->username,$this->password,$this->dbname);
+    }
+
+    public function execute($sql){
+        if(mysqli_query($this->con, $sql)){
+           return 1;     
+        }else
+            return 0;
+    }
+
+    public function __destruct(){
+        mysqli_close($this->con);
+    }
+}
+?>
